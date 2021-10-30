021ad3167f4f7d81d6ebb77159baedea833adab2
Alexey <zoom-alex@yandex.ru>
2021-10-30 18:47:15 +0300

    Add files

---
+++ Ex_3.2.py
@@ -0,0 +1,5 @@
+def my_func(name, surname, byear, city, email, phone):
+    print(name, surname, byear, city, email, phone)
+
+
+my_func(name='Maria', surname='Ivanova', byear=1989, city='Tula', email='email', phone='026565')
